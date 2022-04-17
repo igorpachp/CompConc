3451d54f823910603d1369de32be529b4824215d
igorpachp <igorpacheco9@gmail.com>
2022-04-17 04:42:48 -0300

    teste com uma thread

--- M1/Lab1/arraySquare.c
+++ M1/Lab1/arraySquare.c
@@ -8,20 +8,33 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 
-#define ARR_LEN			10
+#define ARR_LEN  10
+#define NTHREADS 1
 
-void array_square(int * arr, unsigned len) {
+typedef struct {
+	int * arr;
+	unsigned len;
+} square_args_t;
+
+void * array_square(void * arg) {
+	square_args_t * args = (square_args_t *) arg;
 		
-	int * tail = arr + len - 1;
-	while (arr <= tail) {
-		*arr *= *arr;
-		arr++;
+	int * tail = args->arr + args->len - 1;
+	while (args->arr <= tail) {
+		*args->arr *= *args->arr;
+		args->arr++;
 	}
+
+	free(arg);
+	pthread_exit(NULL);
 }
 
 int main(void) {
 	int arr[ARR_LEN];
+	pthread_t tid;
+	square_args_t * arg;
 
 	srand(time(NULL));
 	for (int i = 0; i < ARR_LEN; i++) {
@@ -34,7 +47,20 @@ int main(void) {
 	}
 	puts("");
 
-	array_square(&arr[0], ARR_LEN);
+	arg = malloc(sizeof(square_args_t));
+	if (!arg) {
+		printf("--ERRO: malloc()\n");
+		exit(-1);
+	}
+	arg->arr = &arr[0];
+	arg->len = ARR_LEN;
+	
+	if (pthread_create(&tid, NULL, array_square, (void *) arg)) {
+		printf("--ERRO: pthread_create()\n");
+		exit(-1);
+	}
+	
+	pthread_join(tid, NULL);
 
 	printf("vetor depois:\t");
 	for (int i = 0; i < ARR_LEN; i++) {
