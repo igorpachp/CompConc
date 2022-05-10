608b30a9fa27fd3738f26aea6f27d875dc7d1a90
igorpachp <igorpacheco9@gmail.com>
2022-05-09 23:07:42 -0300

    parte concorrente finalizada

--- M1/Lab4/sqrtPrime.c
+++ M1/Lab4/sqrtPrime.c
@@ -8,23 +8,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+#include "../../lib/timer.h"
 
 #define DEFAULT_ARR_SIZE 10 // tamanho padrão do vetor
 #define DEFAULT_NTHREADS 1 // número padrão de threads
 
 typedef enum {False, True} boolean_t;
 
-int * arr = NULL;
-float * roots_seq = NULL;
-float * roots_con = NULL;
-long long unsigned size;
 int nthreads;
 
 boolean_t is_prime(int);
-void init_arr(); // inicializa o vetor aleatoriamente
-void display_arr(); // exibir vetor
+void init_arr(int *, long long unsigned); // inicializa o vetor aleatoriamente
+void sqrt_primes_seq(int *, float *, long long unsigned); // calcula as raízes dos primos sequencialmente
 
 int main(int argc, char * argv[]) {
+    int * arr = NULL;
+    long long unsigned size;
+
+    // variaveis sequenciais
+    double start_s, end_s, elapsed_s;
+    float * roots_seq = NULL;
+
+    // variaveis concorrentes
+    float * roots_con = NULL;
 
     // verificação de entradas
     switch (argc)
@@ -50,26 +57,54 @@ int main(int argc, char * argv[]) {
             exit(-1);
             break;
     }
-    
+
     // alocação de memória
     arr = malloc(sizeof(int) * size);
     if (!arr) {
         fprintf(stderr, "--ERRO: malloc()\n");
         exit(-2);
     }
+    roots_seq = malloc(sizeof(float) * size);
+    if (!roots_seq) {
+        fprintf(stderr, "--ERRO: malloc()\n");
+        exit(-2);
+    }
+    roots_con = malloc(sizeof(float) * size);
+    if (!roots_con) {
+        fprintf(stderr, "--ERRO: malloc()\n");
+        exit(-2);
+    }
 
     // inicializando o vetor
-    init_arr();
+    init_arr(arr, size);
 
-    // exibindo vetor
-    display_arr();
+    // raiz sequencial
+    GET_TIME(start_s);
+    sqrt_primes_seq(arr, roots_seq, size);
+    GET_TIME(end_s);
+    elapsed_s = end_s - start_s;
+
+    // exibindo vetores
     for (long long unsigned i = 0; i < size; i++) {
-        printf("%s ", is_prime(*(arr + i)) ? "sim" : "nao");
+        printf("%d ", *(arr + i));
+    }
+    printf("\n");
+    for (long long unsigned i = 0; i < size; i++) {
+        printf("%.2f ", *(roots_seq + i));
+    }
+    printf("\n");
+    for (long long unsigned i = 0; i < size; i++) {
+        printf("%.2f ", *(roots_con + i));
     }
     printf("\n");
 
+    // medindo tempo
+    printf("Tempo usado pela funcao sequencial: %.6lf\n", elapsed_s);
+
     // liberando memória
     free(arr);
+    free(roots_seq);
+    free(roots_con);
 
     return 0;
 }
@@ -95,26 +130,31 @@ boolean_t is_prime(int num) {
 }
 
 /* 
- * Esta função inicializa os elementos do vetor de forma aleatória.
+ * Esta função procura por números primos em um 
+ * vetor de inteiros e calcula suas raízes.
+ * Entradas esperadas:
+ *   num --> Um número inteiro
+ * Saída:
+ *   True --> Caso seja primo.
+ *   False --> Caso contrário.
  */
-void init_arr() {
-    long long unsigned arr_size = size;
-    srand(time(NULL));
-
-    while (arr_size--) {
-        *(arr + arr_size) = rand() % 101;
+void sqrt_primes_seq(int * arr, float * out, long long unsigned size) {
+    while(size--) {
+        if (is_prime(arr[size])) out[size] = sqrt(arr[size]);
+        else out[size] = arr[size];
     }
 }
 
 /* 
- * Esta função exibe um vetor.
+ * Esta função inicializa os elementos do vetor de forma aleatória.
  * Entradas esperadas:
- *   arr --> Ponteiro para float
- *   size --> O tamanho do vetor
+ *   arr --> Um vetor de inteiros.
+ *   size --> O tamanho do vetor.
  */
-void display_arr() {
-    for (long long unsigned i = 0; i < size; i++) {
-        printf("%d ", *(arr + i));
+void init_arr(int * arr, long long unsigned size) {
+    srand(time(NULL));
+
+    while (size--) {
+        *(arr + size) = rand() % 101;
     }
-    printf("\n");
 }
