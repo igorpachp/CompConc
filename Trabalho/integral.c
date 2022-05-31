2066b46fce1e2a9c9055e0e91c2d07b824347cb4
igorpachp <igorpacheco9@gmail.com>
2022-05-31 16:31:49 -0300

    teste de funcionamento da função concorrente

--- Trabalho/integral.c
+++ Trabalho/integral.c
@@ -14,6 +14,7 @@ double reference_function(double x) {
 int main() {
     double x[] = {-1.0, -0.8, -0.6, -0.4, -0.2, 0.0, 0.2, 0.4, 0.6, 0.8, 1.0};
     double y[sizeof(x) / sizeof(double)];
+    pthread_t tid[NTHREADS];
 
     for (int i = 0; i < sizeof(x) / sizeof(double); i++) {
         y[i] = reference_function(x[i]);
@@ -23,31 +24,32 @@ int main() {
     printf("%lf\n", integral_continua_sequencial(reference_function, 10, -1, 1));
     printf("%lf\n", integral_continua_com_precisao_sequencial(reference_function, -1, 1, 0.01, 4.62));
 
-    IDiscreta_args_t * args = (IDiscreta_args_t *) malloc(sizeof(IDiscreta_args_t));
-    if (!args) {
-        fprintf(stderr, "--ERRO: malloc()\n");
-        exit(-1);
+    for (int i = 0; i < NTHREADS; i++) {
+        IDiscreta_args_t * args = (IDiscreta_args_t *) malloc(sizeof(IDiscreta_args_t));
+        if (!args) {
+            fprintf(stderr, "--ERRO: malloc()\n");
+            exit(-1);
+        }
+
+        args->size = sizeof(x) / sizeof(double);
+        args->tid = i;
+        args->x = &x;
+        args->y = &y;
+        pthread_create(tid + i, NULL, integral_discreta_concorrente, args);
     }
 
-    args->size = sizeof(x) / sizeof(double);
-    args->tid = 0;
-    args->x = &x;
-    args->y = &y;
-
-    pthread_t tid;
-    pthread_create(&tid, NULL, integral_discreta_concorrente, args);
-    double * integral = (double *) malloc(sizeof(double));
-    if (!integral) {
-        fprintf(stderr, "--ERRO: malloc()\n");
-        exit(-1);
+    double integral = 0;
+    double * resultado;
+    for (int i = 0; i < NTHREADS; i++) {
+        if (pthread_join(*(tid + i), (void **) &resultado)) {
+            fprintf(stderr, "--ERRO: pthread_join()\n");
+            exit(-4);
+        }
+        integral += *resultado;
     }
+    printf("%lf\n", integral);
 
-    if (pthread_join(tid, (void **) &integral)) {
-        fprintf(stderr, "--ERRO: pthread_join()\n");
-        exit(-4);
-    }
-    printf("%lf\n", *integral);
-    free(integral);
+    free(resultado);
 
     return 0;
 }
